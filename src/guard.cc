3afe44c38e6dcf88ebb11759f2e4481d8156036b
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2017-03-21 13:22:56 +0000

    Use ATOMIC_SWAP in guard.cc

--- src/guard.cc
+++ src/guard.cc
@@ -71,19 +71,19 @@
 // ARM ABI - 32-bit guards.
 typedef uint32_t guard_t;
 typedef uint32_t guard_lock_t;
-static const uint32_t LOCKED = static_cast<guard_t>(1) << 31;
-static const uint32_t INITIALISED = 1;
+static uint32_t LOCKED = static_cast<guard_t>(1) << 31;
+static uint32_t INITIALISED = 1;
 #define LOCK_PART(guard) (guard)
 #define INIT_PART(guard) (guard)
 #elif defined(_LP64)
 typedef uint64_t guard_t;
 typedef uint64_t guard_lock_t;
 #	if defined(__LITTLE_ENDIAN__)
-static const guard_t LOCKED = static_cast<guard_t>(1) << 63;
-static const guard_t INITIALISED = 1;
+static guard_t LOCKED = static_cast<guard_t>(1) << 63;
+static guard_t INITIALISED = 1;
 #	else
-static const guard_t LOCKED = 1;
-static const guard_t INITIALISED = static_cast<guard_t>(1) << 56;
+static guard_t LOCKED = 1;
+static guard_t INITIALISED = static_cast<guard_t>(1) << 56;
 #	endif
 #define LOCK_PART(guard) (guard)
 #define INIT_PART(guard) (guard)
@@ -94,28 +94,28 @@ typedef struct {
 	uint32_t init_half;
 	uint32_t lock_half;
 } guard_t;
-static const uint32_t LOCKED = static_cast<guard_lock_t>(1) << 31;
-static const uint32_t INITIALISED = 1;
+static uint32_t LOCKED = static_cast<guard_lock_t>(1) << 31;
+static uint32_t INITIALISED = 1;
 #	else
 typedef struct {
 	uint32_t init_half;
 	uint32_t lock_half;
 } guard_t;
 static_assert(sizeof(guard_t) == sizeof(uint64_t), "");
-static const uint32_t LOCKED = 1;
-static const uint32_t INITIALISED = static_cast<guard_lock_t>(1) << 24;
+static uint32_t LOCKED = 1;
+static uint32_t INITIALISED = static_cast<guard_lock_t>(1) << 24;
 #	endif
 #define LOCK_PART(guard) (&(guard)->lock_half)
 #define INIT_PART(guard) (&(guard)->init_half)
 #endif
-static const guard_lock_t INITIAL = 0;
+static guard_lock_t INITIAL = 0;
 
 /**
  * Acquires a lock on a guard, returning 0 if the object has already been
  * initialised, and 1 if it has not.  If the object is already constructed then
  * this function just needs to read a byte from memory and return.
  */
-extern "C" int __cxa_guard_acquire(volatile guard_t *guard_object)
+extern "C" int __cxa_guard_acquire(guard_t *guard_object)
 {
 	guard_lock_t old;
 	// Not an atomic read, doesn't establish a happens-before relationship, but
@@ -130,8 +130,7 @@ extern "C" int __cxa_guard_acquire(volatile guard_t *guard_object)
 		// Loop trying to move the value of the guard from 0 (not
 		// locked, not initialised) to the locked-uninitialised
 		// position.
-		old = __sync_val_compare_and_swap(LOCK_PART(guard_object),
-		    INITIAL, LOCKED);
+    old = ATOMIC_SWAP(LOCK_PART(guard_object), LOCKED);
 		if (old == INITIAL) {
 			// Lock obtained.  If lock and init bit are
 			// in separate words, check for init race.
@@ -166,28 +165,22 @@ extern "C" int __cxa_guard_acquire(volatile guard_t *guard_object)
  * Releases the lock without marking the object as initialised.  This function
  * is called if initialising a static causes an exception to be thrown.
  */
-extern "C" void __cxa_guard_abort(volatile guard_t *guard_object)
+extern "C" void __cxa_guard_abort(guard_t *guard_object)
 {
-	__attribute__((unused))
-	bool reset = __sync_bool_compare_and_swap(LOCK_PART(guard_object),
-	    LOCKED, INITIAL);
-	assert(reset);
+	ATOMIC_SWAP(LOCK_PART(guard_object), INITIAL);
 }
 /**
  * Releases the guard and marks the object as initialised.  This function is
  * called after successful initialisation of a static.
  */
-extern "C" void __cxa_guard_release(volatile guard_t *guard_object)
+extern "C" void __cxa_guard_release(guard_t *guard_object)
 {
 	guard_lock_t old;
 	if (INIT_PART(guard_object) == LOCK_PART(guard_object))
 		old = LOCKED;
 	else
 		old = INITIAL;
-	__attribute__((unused))
-	bool reset = __sync_bool_compare_and_swap(INIT_PART(guard_object),
-	    old, INITIALISED);
-	assert(reset);
+	ATOMIC_SWAP(INIT_PART(guard_object), INITIALISED);
 	if (INIT_PART(guard_object) != LOCK_PART(guard_object))
 		*LOCK_PART(guard_object) = INITIAL;
 }
